f48949d8ce5d55e207fa9fbae2bf0b2c23a73be4
yueliu <juneslayer@gmail.com>
2018-07-20 13:59:56 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { ConceptComponent } from './concept/concept.component';
+import { EcoComponent } from './eco/eco.component';
+import { DoublechainsComponent } from './doublechains/doublechains.component';
+import { InfraComponent } from './infra/infra.component';
+import { FeaturesComponent } from './features/features.component';
+import { AppsComponent } from './apps/apps.component';
+import { ServicesComponent } from './services/services.component';
+import { TeamComponent } from './team/team.component';
+import { RoadmapComponent } from './roadmap/roadmap.component';
+import {MatButtonModule, MatGridListModule, MatIconModule} from '@angular/material';
+import { InfraTitleComponent } from './infra-title/infra-title.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ConceptComponent,
+    EcoComponent,
+    DoublechainsComponent,
+    InfraComponent,
+    FeaturesComponent,
+    AppsComponent,
+    ServicesComponent,
+    TeamComponent,
+    RoadmapComponent,
+    InfraTitleComponent,
+    FooterComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
